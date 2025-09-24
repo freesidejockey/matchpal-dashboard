bad95e19a6681d897734eb2a502f5f89b4d457ce
Freeside Jockey <freesidejockey@gmail.com>
2025-09-24 10:06:02 -0600

    feat: implemented signin, signup, and signout
    Includes email verification

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -40,12 +40,14 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/signup') &&
+    !request.nextUrl.pathname.startsWith('/signin') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/signup'
     return NextResponse.redirect(url)
   }
 
